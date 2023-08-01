3d015c8c29e67ceb71809df046d1c7a37f7d1720
Sumanone8 <sumanjhabhl@gmail.com>
2023-08-01 23:34:03 +0530

    Suman : Rule3 – Should have at least 1 numeric number in the password - NOTE – All rules must be passed

--- Program.cs
+++ Program.cs
@@ -8,16 +8,29 @@ class Program
         if (password.Length >= 8)
         {
             // Check if the password contains at least one uppercase letter.
+            bool hasUppercase = false;
             foreach (char character in password)
             {
                 if (char.IsUpper(character))
                 {
-                    return true;
+                    hasUppercase = true;
+                    break;
                 }
             }
 
-            // If no uppercase letter is found, return false.
-            return false;
+            // Check if the password contains at least one numeric digit.
+            bool hasNumeric = false;
+            foreach (char character in password)
+            {
+                if (char.IsDigit(character))
+                {
+                    hasNumeric = true;
+                    break;
+                }
+            }
+
+            // Return true only if both uppercase and numeric conditions are met.
+            return hasUppercase && hasNumeric;
         }
         else
         {
@@ -36,7 +49,7 @@ class Program
         }
         else
         {
-            Console.WriteLine("Invalid password. The password must have a minimum of 8 characters and at least one uppercase letter.");
+            Console.WriteLine("Invalid password. The password must have a minimum of 8 characters, at least one uppercase letter, and at least one numeric digit.");
         }
     }
 }
