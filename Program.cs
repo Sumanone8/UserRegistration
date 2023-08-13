f0e60748994e9537a7c531e0f334be33576df7e1
Sumanone8 <sumanjhabhl@gmail.com>
2023-08-13 15:12:32 +0530

    Add parameterized email validation NUnit tests

--- Program.cs
+++ Program.cs
@@ -1,55 +1,45 @@
 ﻿using NUnit.Framework;
+using System;
+using System.Collections;
 using System.Text.RegularExpressions;
 
 [TestFixture]
-public class UserValidationTests
+public class EmailValidationParameterizedTests
 {
-    [TestCase("John", "Doe", "john.doe@example.com", "1234567890", "SecurePassword123")]
-    [TestCase("Jane", "Smith", "jane.smith@example.com", "9876543210", "StrongPassword456")]
-    public void ValidEntries_ShouldReturnTrue(string firstName, string lastName, string email, string mobile, string password)
+    private static IEnumerable TestCases
     {
-        Assert.IsTrue(ValidateUserEntry(firstName, lastName, email, mobile, password));
+        get
+        {
+            yield return new TestCaseData("abc@yahoo.com").Returns(true);
+            yield return new TestCaseData("abc-100@yahoo.com").Returns(true);
+            yield return new TestCaseData("abc.100@yahoo.com").Returns(true);
+            yield return new TestCaseData("abc111@abc.com").Returns(true);
+            yield return new TestCaseData("abc-100@abc.net").Returns(true);
+            yield return new TestCaseData("abc.100@abc.com.au").Returns(true);
+            yield return new TestCaseData("abc@1.com").Returns(true);
+            yield return new TestCaseData("abc@gmail.com.com").Returns(true);
+            yield return new TestCaseData("abc+100@gmail.com").Returns(true);
+
+            yield return new TestCaseData("abc").Returns(false);
+            yield return new TestCaseData("abc@.com.my").Returns(false);
+            yield return new TestCaseData("abc123@gmail.a").Returns(false);
+            yield return new TestCaseData("abc123@.com").Returns(false);
+            yield return new TestCaseData("abc123@.com.com").Returns(false);
+            yield return new TestCaseData(".abc@abc.com").Returns(false);
+            yield return new TestCaseData("abc()*@gmail.com").Returns(false);
+            yield return new TestCaseData("abc@%*.com").Returns(false);
+            yield return new TestCaseData("abc..2002@gmail.com").Returns(false);
+            yield return new TestCaseData("abc.@gmail.com").Returns(false);
+            yield return new TestCaseData("abc@abc@gmail.com").Returns(false);
+            yield return new TestCaseData("abc@gmail.com.1a").Returns(false);
+            yield return new TestCaseData("abc@gmail.com.aa.au").Returns(false);
+        }
     }
 
-    [TestCase("John", "Doe", "invalid-email", "1234567890", "WeakPassword with c# code")]
-    [TestCase("", "Smith", "jane.smith@example.com", "9876543210", "StrongPassword456")]
-    [TestCase("Jane", "", "jane.smith@example.com", "9876543210", "StrongPassword456")]
-    [TestCase("John", "Doe", "john.doe@example.com", "invalid-mobile", "SecurePassword123")]
-    public void InvalidEntries_ShouldReturnFalse(string firstName, string lastName, string email, string mobile, string password)
-    {
-        Assert.IsFalse(ValidateUserEntry(firstName, lastName, email, mobile, password));
-    }
-
-    static bool ValidateUserEntry(string firstName, string lastName, string email, string mobile, string password)
-    {
-        return ValidateName(firstName) &&
-               ValidateName(lastName) &&
-               ValidateEmail(email) &&
-               ValidateMobile(mobile) &&
-               ValidatePassword(password);
-    }
-
-    static bool ValidateName(string name)
-    {
-        // Validate name logic here
-        return !string.IsNullOrEmpty(name);
-    }
-
-    static bool ValidateEmail(string email)
+    [TestCaseSource(nameof(TestCases))]
+    public bool ValidateEmail(string email)
     {
         string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
         return Regex.IsMatch(email, pattern);
-    }
-
-    static bool ValidateMobile(string mobile)
-    {
-        // Validate mobile logic here
-        return !string.IsNullOrEmpty(mobile) && mobile.Length == 10 && long.TryParse(mobile, out _);
-    }
-
-    static bool ValidatePassword(string password)
-    {
-        // Validate password logic here
-        return !string.IsNullOrEmpty(password) && !password.Contains("c# code");
     }
 }
