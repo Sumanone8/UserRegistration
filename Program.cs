368be65b6e8fa218eb7778b4c98d9d12f28d64ae
Sumanone8 <sumanjhabhl@gmail.com>
2023-08-13 15:22:58 +0530

    Refactor code using lambda functions for validation

--- Program.cs
+++ Program.cs
@@ -2,103 +2,77 @@
 using System;
 using System.Text.RegularExpressions;
 
-public class InvalidFirstNameException : Exception
+public class InvalidUserDetailException : Exception
 {
-    public InvalidFirstNameException(string message) : base(message) { }
+    public InvalidUserDetailException(string message) : base(message) { }
 }
 
-public class InvalidLastNameException : Exception
+public class UserValidator
 {
-    public InvalidLastNameException(string message) : base(message) { }
-}
+    public static void ValidateUserEntry(string firstName, string lastName, string email, string mobile, string password)
+    {
+        ValidateDetail(() => !string.IsNullOrEmpty(firstName), "First name is invalid.");
+        ValidateDetail(() => !string.IsNullOrEmpty(lastName), "Last name is invalid.");
+        ValidateDetail(() => IsValidEmail(email), "Email is invalid.");
+        ValidateDetail(() => IsValidMobile(mobile), "Mobile number is invalid.");
+        ValidateDetail(() => IsValidPassword(password), "Password is invalid.");
+    }
 
-public class InvalidEmailException : Exception
-{
-    public InvalidEmailException(string message) : base(message) { }
-}
+    static void ValidateDetail(Func<bool> isValid, string errorMessage)
+    {
+        if (!isValid.Invoke())
+        {
+            throw new InvalidUserDetailException(errorMessage);
+        }
+    }
 
-public class InvalidMobileException : Exception
-{
-    public InvalidMobileException(string message) : base(message) { }
-}
+    static bool IsValidEmail(string email)
+    {
+        string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+        return Regex.IsMatch(email, pattern);
+    }
 
-public class InvalidPasswordException : Exception
-{
-    public InvalidPasswordException(string message) : base(message) { }
+    static bool IsValidMobile(string mobile)
+    {
+        return !string.IsNullOrEmpty(mobile) && mobile.Length == 10 && long.TryParse(mobile, out _);
+    }
+
+    static bool IsValidPassword(string password)
+    {
+        return !string.IsNullOrEmpty(password) && !password.Contains("c# code");
+    }
 }
 
 [TestFixture]
 public class UserValidationTests
 {
-    static void ValidateUserEntry(string firstName, string lastName, string email, string mobile, string password)
-    {
-        ValidateName(firstName);
-        ValidateName(lastName);
-        ValidateEmail(email);
-        ValidateMobile(mobile);
-        ValidatePassword(password);
-    }
-
-    static void ValidateName(string name)
-    {
-        if (string.IsNullOrEmpty(name))
-        {
-            throw new InvalidFirstNameException("First name is invalid.");
-        }
-    }
-
-    static void ValidateEmail(string email)
-    {
-        string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
-        if (!Regex.IsMatch(email, pattern))
-        {
-            throw new InvalidEmailException("Email is invalid.");
-        }
-    }
-
-    static void ValidateMobile(string mobile)
-    {
-        if (string.IsNullOrEmpty(mobile) || mobile.Length != 10 || !long.TryParse(mobile, out _))
-        {
-            throw new InvalidMobileException("Mobile number is invalid.");
-        }
-    }
-
-    static void ValidatePassword(string password)
-    {
-        if (string.IsNullOrEmpty(password) || password.Contains("c# code"))
-        {
-            throw new InvalidPasswordException("Password is invalid.");
-        }
-    }
-
     [Test]
     public void ValidEntries_ShouldNotThrowExceptions()
     {
-        Assert.DoesNotThrow(() => ValidateUserEntry("John", "Doe", "john.doe@example.com", "1234567890", "StrongPassword123"));
+        Assert.DoesNotThrow(() => UserValidator.ValidateUserEntry("John", "Doe", "john.doe@example.com", "1234567890", "StrongPassword123"));
     }
 
     [Test]
-    public void InvalidFirstName_ShouldThrowInvalidFirstNameException()
+    public void InvalidFirstName_ShouldThrowInvalidUserDetailException()
     {
-        Assert.Throws<InvalidFirstNameException>(() => ValidateUserEntry("", "Smith", "jane.smith@example.com", "9876543210", "StrongPassword456"));
+        Assert.Throws<InvalidUserDetailException>(() => UserValidator.ValidateUserEntry("", "Smith", "jane.smith@example.com", "9876543210", "StrongPassword456"));
     }
 
     [Test]
-    public void InvalidEmail_ShouldThrowInvalidEmailException()
+    public void InvalidEmail_ShouldThrowInvalidUserDetailException()
     {
-        Assert.Throws<InvalidEmailException>(() => ValidateUserEntry("Jane", "Smith", "invalid-email", "9876543210", "StrongPassword456"));
+        Assert.Throws<InvalidUserDetailException>(() => UserValidator.ValidateUserEntry("Jane", "Smith", "invalid-email", "9876543210", "StrongPassword456"));
     }
 
     [Test]
-    public void InvalidMobile_ShouldThrowInvalidMobileException()
+    public void InvalidMobile_ShouldThrowInvalidUserDetailException()
     {
-        Assert.Throws<InvalidMobileException>(() => ValidateUserEntry("John", "Doe", "john.doe@example.com", "invalid-mobile", "StrongPassword123"));
+        Assert.Throws<InvalidUserDetailException>(() => UserValidator.ValidateUserEntry("John", "Doe", "john.doe@example.com", "invalid-mobile", "StrongPassword123"));
     }
 
     [Test]
-    public void InvalidPassword_ShouldThrowInvalidPasswordException()
+    public void InvalidPassword_ShouldThrowInvalidUserDetailException()
     {
-        Assert.Throws<InvalidPasswordException>(() => ValidateUserEntry("John", "Doe", "john.doe@example.com", "1234567890", "WeakPassword with c# code"));
+        Assert.Throws<InvalidUserDetailException>(() => UserValidator.ValidateUserEntry("John", "Doe", "john.doe@example.com", "1234567890", "WeakPassword with c# code"));
     }
 }
